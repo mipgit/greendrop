e588b5f5db77c2c159562047fd565cf956f4b51a
constancalferreira <161485742+constancalferreira@users.noreply.github.com>
2025-04-18 16:23:41 +0100

    Added groups segment to the navbar that directs to the Group Page (to be implemented)

---
+++ greendrop/lib/view/groups/groups_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GroupsView extends StatelessWidget {
+  const GroupsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'Group Page to be implemented',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+}
